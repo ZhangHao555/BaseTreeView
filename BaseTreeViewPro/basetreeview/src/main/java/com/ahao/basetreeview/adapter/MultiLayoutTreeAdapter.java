a6a4b814e1b071b8e6ba395208d37835693e0dc7
ZhangHao <zhanghao@feifanuniv.com>
2018-10-15 17:29:23 +0800

    optimization

---
+++ BaseTreeViewPro/basetreeview/src/main/java/com/ahao/basetreeview/adapter/MultiLayoutTreeAdapter.java
@@ -0,0 +1,87 @@
+package com.ahao.basetreeview.adapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ahao.basetreeview.model.NodeId;
+import com.ahao.basetreeview.model.TreeNode;
+import com.ahao.basetreeview.util.DpUtil;
+import com.ahao.basetreeview.util.TreeUtils;
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+
+public class MultiLayoutTreeAdapter<T extends NodeId> extends BaseMultiItemQuickAdapter<TreeNode<T>, BaseViewHolder> {
+
+    public interface OnTreeClickedListener<T extends NodeId> {
+
+        void onNodeClicked(View view, TreeNode<T> node, int position);
+
+        void onLeafClicked(View view, TreeNode<T> node, int position);
+    }
+
+    private List<TreeNode<T>> dataToBind;
+    private List<TreeNode<T>> allData;
+
+    private OnTreeClickedListener onTreeClickedListener;
+
+    public MultiLayoutTreeAdapter(final List<TreeNode<T>> dataToBind, final List<TreeNode<T>> allData) {
+        super(dataToBind);
+
+        this.dataToBind = dataToBind;
+        this.allData = allData;
+
+        addItemTypes();
+
+        setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                TreeNode<T> node = dataToBind.get(position);
+                if (!node.isLeaf()) {
+                    List<TreeNode<T>> l = TreeUtils.getNodeChildren(node);
+
+                    if(node.isExpand()){
+                        dataToBind.removeAll(l);
+                        node.setExpand(false);
+                        notifyItemRangeRemoved(position,l.size());
+                    }else{
+                        dataToBind.addAll(position + 1,l);
+                        node.setExpand(true);
+                        notifyItemRangeInserted(position,l.size());
+                    }
+                    node.setExpand(!node.isExpand());
+
+                    if (onTreeClickedListener != null) {
+                        onTreeClickedListener.onNodeClicked(view, node, position);
+                    }
+                } else {
+                    if (onTreeClickedListener != null) {
+                        onTreeClickedListener.onLeafClicked(view, node, position);
+                    }
+                }
+
+            }
+        });
+    }
+
+    protected void addItemTypes() {
+
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, TreeNode<T> item) {
+        int level = item.getLevel();
+        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) helper.itemView.getLayoutParams();
+        layoutParams.leftMargin = getTreeNodeMargin() * level;
+    }
+
+    public void setOnTreeClickedListener(OnTreeClickedListener onTreeClickedListener) {
+        this.onTreeClickedListener = onTreeClickedListener;
+    }
+
+    protected int getTreeNodeMargin() {
+        return DpUtil.dip2px(this.mContext, 10);
+    }
+}
---
+++ BaseTreeViewPro/basetreeview/src/main/java/com/ahao/basetreeview/model/NodeId.java
@@ -0,0 +1,7 @@
+package com.ahao.basetreeview.model;
+public interface NodeId {
+
+    public String getId();
+
+    public String getPId();
+}
