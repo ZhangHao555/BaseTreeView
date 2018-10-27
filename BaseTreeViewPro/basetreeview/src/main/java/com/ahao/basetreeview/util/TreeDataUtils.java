4efa161f2aaf376964dbe17ab1e80ed196e28c1f
ahao <zhangnnyyy@126.com>
2018-10-27 15:00:38 +0800

    优化

--- BaseTreeViewPro/basetreeview/src/main/java/com/ahao/basetreeview/util/TreeDataUtils.java
+++ BaseTreeViewPro/basetreeview/src/main/java/com/ahao/basetreeview/util/TreeDataUtils.java
@@ -6,6 +6,7 @@ import com.ahao.basetreeview.model.NodeId;
 import com.ahao.basetreeview.model.TreeNode;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,28 +14,26 @@ public class TreeDataUtils {
 
     public static <T extends NodeId> List<TreeNode<T>> convertDataToTreeNode(List<T> datas, int maxViewType) {
         List<TreeNode<T>> nodes = new ArrayList<>();
-        List<TreeNode<T>> rootNode = new ArrayList<>();
         Map<String, TreeNode<T>> map = new HashMap();
 
         for (NodeId nodeId : datas) {
             TreeNode treeNode = new TreeNode(nodeId, maxViewType);
             nodes.add(treeNode);
             map.put(nodeId.getId(), treeNode);
-            if(TextUtils.isEmpty(treeNode.getPId())){
-                rootNode.add(treeNode);
-            }
         }
 
-        for(TreeNode<T> treeNode : nodes){
+        Iterator<TreeNode<T>> iterator = nodes.iterator();
+        while(iterator.hasNext()){
+            TreeNode<T> treeNode = iterator.next();
             String pId = treeNode.getPId();
             TreeNode<T> parentNode = map.get(pId);
             if (parentNode != null) {
                 parentNode.getChildren().add(treeNode);
                 treeNode.setParent(parentNode);
+                iterator.remove();
             }
         }
-
-        return rootNode;
+        return nodes;
     }
 
     public static <T extends NodeId> List<TreeNode<T>> convertDataToTreeNode(List<T> datas) {
